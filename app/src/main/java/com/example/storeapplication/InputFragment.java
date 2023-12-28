881f16c0f2ce12a6d9f69414275b09cf01d527f8
Thewolf216 <91385614+Thewolf216@users.noreply.github.com>
2023-12-28 07:44:04 +0200

    DSDSA

--- app/src/main/java/com/example/storeapplication/InputFragment.java
+++ app/src/main/java/com/example/storeapplication/InputFragment.java
@@ -7,58 +7,42 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link InputFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class InputFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public InputFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InputFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InputFragment newInstance(String param1, String param2) {
-        InputFragment fragment = new InputFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private EditText nameET;
+    private EditText priceET;
+    private Button addBTN;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_input, container, false);
+        View view = inflater.inflate(R.layout.fragment_input, container, false);
+
+        nameET = view.findViewById(R.id.nameET);
+        priceET = view.findViewById(R.id.priceET);
+        addBTN = view.findViewById(R.id.addBTN);
+        addBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameET.getText().toString();
+                String price = priceET.getText().toString();
+
+                if(name.isEmpty() || price.isEmpty())
+                {
+                    Toast.makeText(requireContext(), "Fill inputs", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    //TODO: send name and price to Recycleview or DB
+                }
+            }
+        });
+
+        return view;
     }
 }
